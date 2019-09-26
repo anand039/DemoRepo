72089393cf6f0aed829f2a08654455ec5afcd82a
USER <anandsingh039@gmail.com>
2019-09-26 22:40:53 +0530

    this is from asian time zone

--- src/test/java/qaclickacademy/com/RESTAPITest.java
+++ src/test/java/qaclickacademy/com/RESTAPITest.java
@@ -3,16 +3,15 @@ package qaclickacademy.com;
 import org.testng.annotations.Test;
 
 public class RESTAPITest {
-	
+
 	@Test
-	public void postJira()
-	{
+	public void postJira() {
 		System.out.println("post jira");
+		System.out.println("post jira123");
 	}
-	
+
 	@Test
-	public void deleteTwiter()
-	{
+	public void deleteTwiter() {
 		System.out.println("delete twiter");
 	}
 
