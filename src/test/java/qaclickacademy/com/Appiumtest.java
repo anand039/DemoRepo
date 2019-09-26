c87d9659824758b48db2a9ee53a9d925add0dbcd
USER <anandsingh039@gmail.com>
2019-09-26 16:07:09 +0530

    first commit

---
+++ src/test/java/qaclickacademy/com/Appiumtest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.com;
+
+import org.testng.annotations.Test;
+
+public class Appiumtest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("native app android");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println(" ios apps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/com/RESTAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.com;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("post jira");
+	}
+	
+	@Test
+	public void deleteTwiter()
+	{
+		System.out.println("delete twiter");
+	}
+
+}
